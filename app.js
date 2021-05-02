8bda0018eb8b1e0b98164d64988b9e90a1f35584
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-05-02 13:46:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-alert("This special edition of this game is dedicated to my special bro ASHFAQUL MAJID");
+
 alert("GAME RULES\n\n\t- The game has 2 players, playing in rounds\n\t- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score\n\t- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn\n\t- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn\n\t- The first player to reach the winning points on GLOBAL score wins the game.\n\nIf you want to see the rules again, just simply reload this page.\n\nBefore starting this game enter your goal score in the bottom 'Winning scorre' field and click on 'Roll Dice' button\n\n\n");
 
 var scores, roundScore, activePlayer, gamePlaying;
@@ -28,7 +28,7 @@ function init() {
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     
-    document.getElementById('name-0').textContent = 'Ashfaq';
+    document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
     
     document.querySelector('.player-0-panel').classList.remove('active');
